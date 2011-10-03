97c8c0f77085420e61ad094ce28cae37e41245db
Iain Dooley <iain@workingsoftware.com.au>
2011-10-03 23:04:33 +1100

    Updated to match new nameless fixture style in murphy

--- plusql.class.php.murphy/fixture.php
+++ plusql.class.php.murphy/fixture.php
@@ -3,7 +3,7 @@
     * field1 | field2
     * value1 | value2
     */
-    murphy\Fixture::add('Some non db business',function($data)
+    murphy\Fixture::add(function($data)
     {
         print_r($data);
     });
