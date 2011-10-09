44f6cf755533eb3473d52d6a9beb63fef4e3cb49
Iain Dooley <iain@workingsoftware.com.au>
2011-10-09 23:50:55 +1100

    Added test for table inspector worker

---
+++ table_inspector_worker.class.php.murphy/default.run.php
@@ -0,0 +1,43 @@
+<?php
+    namespace plusql;
+    
+    \murphy\Test::add(function($runner)
+    {
+        $conn = NULL;
+        \murphy\Fixture::load(dirname(__FILE__).'/../on_clause.class.php.murphy/fixture.php')
+        ->also(dirname(__FILE__).'/../query_iterator.class.php.murphy/fixture.php')
+        ->execute(function($aliases) use(&$conn)
+        {
+            $aliases = $aliases['plusql'];
+            $host = $aliases[0];
+            $username = $aliases[1];
+            $password = $aliases[2];
+            $dbname = $aliases[3];
+            $conn = new Connection($host,$username,$password,$dbname);
+            $conn->connect();
+        });
+        
+        $worker = new TableInspectorWorker('strong_guy',$conn->link());
+        $expected = 'bd3251f7c5acccb2b73fd83be63c7ea2';
+
+        ob_start();
+        print_r($worker->allFields());
+        $actual = md5(trim(ob_get_clean()));
+
+        if($actual == $expected)
+            $runner->pass();
+        else
+            $runner->fail('Did not get the correct field data from TableInspectorWorker');
+
+        $worker = new TableInspectorWorker('strong_guy',$conn->link());
+        $expected = '1b656c3c43833bf1ac9cf5620643522f';
+
+        ob_start();
+        print_r($worker->primaryKeys());
+        $actual = md5(trim(ob_get_clean()));
+        
+        if($actual == $expected)
+            $runner->pass();
+        else
+            $runner->fail('TableInspectorWorker::primaryKeys did not return the correct fields');
+    });
