618b8117152a339be0024c3eb8b84c6b208db609
Iain Dooley <iain@workingsoftware.com.au>
2012-01-12 16:06:31 +1100

    Added a default autoload configuration file so folks can use this easily with any other framework

--- autoload.config.php
+++ autoload.config.php
@@ -1,4 +1,28 @@
 <?php
+    namespace plusql;
+
     /**
-    * This file provides a default autoload implementation for those not using RocketSled
+    * This file provides a default autoload implementation for those not using https://github.com/iaindooley/RocketSled
     */
+    spl_autoload_register(function($class)
+    {
+        //IF THIS IS A NAMESPACED CLASS AND IT'S PART OF plusql
+        if((strpos($class,'\\') !== FALSE) && (strpos($class,'plusql') === 0))
+        {
+            $split = explode('\\',$class);
+            array_shift($split);
+            $file = classToFile(array_pop($split));
+            $filename = dirname(__FILE__).'/'.implode('/',$split).'/'.$file;
+        }
+
+        else
+            $filename = dirname(__FILE__).'/'.classToFile($class);
+        
+        if(file_exists($filename))
+            require_once($filename);
+    });
+
+    function classToFile($class)
+    {
+        return strtolower(ltrim(preg_replace('/([A-Z])/','_\1',$class),'_')).'.class.php';
+    }
