f301be22ec444536154f6472921b1a77fd44cd0c
Iain Dooley <iain@workingsoftware.com.au>
2012-01-13 11:00:32 +1100

    Bumped the rp version

--- rocketpack.config.php
+++ rocketpack.config.php
@@ -1,9 +1,9 @@
 <?php
-    rocketpack\Install::package('PluSQL',array(0,0,0));
+    rocketpack\Install::package('PluSQL',array(0,1,0));
     
     rocketpack\Dependencies::register(function()
     {
         rocketpack\Dependency::forPackage('PluSQL')
-        ->add('Murphy',array(0,0,0))
+        ->add('Murphy',array(0,1,0))
         ->verify();
     });
