8329632bcb04b0393322769b2f270d1e6af7e0d4
Iain Dooley <iain@workingsoftware.com.au>
2012-07-04 16:23:54 +1000

    Bumped version and dependencies

--- rocketpack.config.php
+++ rocketpack.config.php
@@ -1,9 +1,9 @@
 <?php
-    RocketPack\Install::package('https://github.com/iaindooley/PluSQL',array(0,2,1));
+    RocketPack\Install::package('https://github.com/iaindooley/PluSQL',array(0,2,2));
     
     RocketPack\Dependencies::register(function()
     {
         RocketPack\Dependency::forPackage('https://github.com/iaindooley/PluSQL')
-        ->add('https://github.com/iaindooley/Murphy',array(0,2,0))
+        ->add('https://github.com/iaindooley/Murphy',array(0,2,1))
         ->verify();
     });
