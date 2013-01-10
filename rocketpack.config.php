e9e9b7b80e80e5cf461f582decce7183687538c8
Iain Dooley <iain@workingsoftware.com.au>
2013-01-11 00:38:17 +1100

    Corrected rocketpack.config.php for new style RocketSled

--- rocketpack.config.php
+++ rocketpack.config.php
@@ -1,9 +1,10 @@
 <?php
-    RocketPack\Install::package('https://github.com/iaindooley/PluSQL',array(0,0,0));
+    RocketPack\Install::package('https://github.com/iaindooley/PluSQL',array(0,2,3));
     
     RocketPack\Dependencies::register(function()
     {
         RocketPack\Dependency::forPackage('https://github.com/iaindooley/PluSQL')
+        ->into(RocketSled::rs_dir())
         ->add('https://github.com/iaindooley/Murphy',array(0,2,2))
         ->verify();
     });
