f82c95bdd343ab8365e5340edeb90f9a766a98e2
Iain Dooley <iain@workingsoftware.com.au>
2013-10-30 08:17:40 +1100

    Updated rocketpack format for new, standalone RocketPack

--- rocketpack.config.php
+++ rocketpack.config.php
@@ -1,9 +1,7 @@
 <?php
-    RocketPack\Install::package('https://github.com/iaindooley/PluSQL',array(0,0,0));
-    
-    RocketPack\Dependencies::register(function()
+    RocketPack\Dependency::register(function()
     {
-        RocketPack\Dependency::forPackage('https://github.com/iaindooley/PluSQL')
-        ->add('https://github.com/iaindooley/Murphy',array(0,2,2))
-        ->verify();
+        RocketPack\Dependency::into(__DIR__.'/../')
+        ->add('https://github.com/iaindooley/Murphy')
+        ;
     });
