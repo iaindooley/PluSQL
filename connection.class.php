e2f44fe8329a0090d5c60be0d94d70f6a8fba891
Iain Dooley <iain@workingsoftware.com.au>
2014-06-23 14:46:24 +1000

    Added code to automatically re-establish lost connection

--- connection.class.php
+++ connection.class.php
@@ -10,7 +10,11 @@
         public function __construct($host,$user,$password,$dbname)
         {
             $this->credentials = func_get_args();
-            
+            $this->establishConnection($host,$user,$password,$dbname)
+        }
+        
+        private function establishConnection($host,$user,$password,$dbname)
+        {
             if(class_exists('mysqli'))
                 $this->link = new mysqli($host,$user,$password);
             else
@@ -26,12 +30,35 @@
         {
             if($this->link instanceof mysqli)
             {
+                if(!$this->link->ping())
+                {
+                    if(ini_get('mysqli.reconnect'))
+                        throw new Exception('MySQL server connection lost and unable to reconnect');
+                    else
+                    {
+                        $this->link->close();
+                        $this->establishConnection($this->credentials[Plusql::HOST],
+                                                   $this->credentials[Plusql::USER],
+                                                   $this->credentials[Plusql::PASS],
+                                                   $this->credentials[Plusql::NAME]);
+                    }
+                }
+
                 if(!$this->link->select_db($this->credentials[Plusql::NAME]))
                     throw new Exception($this->link->error);
             }
 
             else
             {
+                if(!mysql_ping($this->link))
+                {
+                    mysql_close($this->link);
+                    $this->establishConnection($this->credentials[Plusql::HOST],
+                                               $this->credentials[Plusql::USER],
+                                               $this->credentials[Plusql::PASS],
+                                               $this->credentials[Plusql::NAME]);
+                }
+
                 if(!mysql_select_db($this->credentials[Plusql::NAME],$this->link))
                     throw new Exception(mysql_error());
             }
